5a9a6c4c63844e2f1a776fa5f9669c6cc1d05adb
alanaainez <alainez649@gmail.com>
2025-03-20 18:23:14 -0400

    git commit -m "Adjustments to run server"

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,28 @@
-import express from 'express';
+import express, { Application } from 'express';
 import path from 'node:path';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import db from './config/connection.js';
 import typeDefs from './schemas/typeDefs.js';
 import resolvers from './schemas/resolvers.js'
 import routes from './routes/index.js';
 
-const app = express();
+const app: Application = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+  
+  app.use('/graphql', expressMiddleware(server, { context: async() => ({}) }));
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -25,5 +32,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+    console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+}
+
+startServer();
