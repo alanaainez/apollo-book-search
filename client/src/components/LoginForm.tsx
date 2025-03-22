41b4817ec16e88219b7a48af9e1a45ac938de959
alanaainez <alainez649@gmail.com>
2025-03-22 16:05:16 -0400

    Update Login for Apollo

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -2,16 +2,30 @@
 import { useState } from 'react';
 import type { ChangeEvent, FormEvent } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 import type { User } from '../models/User';
+import { Book } from '../models/Book';
 
 // biome-ignore lint/correctness/noEmptyPattern: <explanation>
 const LoginForm = ({}: { handleModalClose: () => void }) => {
-  const [userFormData, setUserFormData] = useState<User>({ username: '', email: '', password: '', savedBooks: [] });
+  const [userFormData, setUserFormData] = useState<User>({ 
+    username: '', email: '', password: '', savedBooks: [] });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [loginMutation] = useMutation(LOGIN_USER);
+
+  const login = async ({ variables }: { variables: { username: string | null; email: string | null; password: string | null; savedBooks: Book[]; } }) => {
+    try {
+      const response = await loginMutation({ variables });
+      return response;
+    } catch (error) {
+      console.error('Error during login mutation:', error);
+      throw error;
+    }
+  };
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -29,14 +43,15 @@ const LoginForm = ({}: { handleModalClose: () => void }) => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data, errors } = await login({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
+      if (errors) {
         throw new Error('something went wrong!');
       }
 
-      const { token } = await response.json();
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -93,3 +108,5 @@ const LoginForm = ({}: { handleModalClose: () => void }) => {
 };
 
 export default LoginForm;
+
+
