f35c214166b9003760b7551595fa1183e55ae42f
alanaainez <alainez649@gmail.com>
2025-03-22 15:38:49 -0400

    Added GET_ME

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -1,1 +1,19 @@
+import { gql } from '@apollo/client';
+
 // GET_ME query
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+        }
+    }
+}`;
